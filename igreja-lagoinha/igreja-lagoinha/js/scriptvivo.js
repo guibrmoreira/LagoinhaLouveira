93b560b39516c9aa758abbb9da9022b8a3b39ddd
guibrmoreira <168133367+guibrmoreira@users.noreply.github.com>
2025-07-09 19:08:51 -0300

    Add files via upload

--- igreja-lagoinha/igreja-lagoinha/js/scriptvivo.js
+++ igreja-lagoinha/igreja-lagoinha/js/scriptvivo.js
@@ -1,39 +1,55 @@
 const channelId = "UCJ6sOzgtvuCtnxb37xYWKiw";
-const apiKey = "AIzaSyBunP_5CVz0l8hckYgrv1yZ5Aw5UVl1OBI"; 
+const apiKey = "AIzaSyBunP_5CVz0l8hckYgrv1yZ5Aw5UVl1OBI";
 
 async function carregarVideo() {
-    const videoContainer = document.getElementById("video-container");
+  const videoContainer = document.getElementById("video-container");
 
-    try {
-        const liveURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&eventType=live&type=video&key=${apiKey}`;
-        const liveResponse = await fetch(liveURL);
-        const liveData = await liveResponse.json();
+  try {
+    // 1. Buscar vídeo AO VIVO
+    const liveURL = `https://www.googleapis.com/youtube/v3/search?part=snippet,id&channelId=${channelId}&eventType=live&type=video&key=${apiKey}`;
+    const liveResponse = await fetch(liveURL);
+    const liveData = await liveResponse.json();
+    console.log("Resposta ao vivo:", liveData);
 
-        if (liveData.items && liveData.items.length > 0) {
-            const liveId = liveData.items[0].id.videoId;
-            videoContainer.innerHTML = `
-                <iframe width="100%" height="400" src="https://www.youtube.com/embed/${liveId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                <p style="text-align:center; color:green;">🔴 Ao Vivo</p>
-            `;
-        } else {
-            const lastVideoURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=1&order=date&type=video&key=${apiKey}`;
-            const lastResponse = await fetch(lastVideoURL);
-            const lastData = await lastResponse.json();
-
-            if (lastData.items && lastData.items.length > 0) {
-                const videoId = lastData.items[0].id.videoId;
-                videoContainer.innerHTML = `
-                    <iframe src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                    <p style="text-align:center; color:gray;">⚪ Última Transmissão</p>
-                `;
-            } else {
-                videoContainer.innerHTML = "<p>Nenhum vídeo encontrado no canal.</p>";
-            }
-        }
-    } catch (erro) {
-        console.error("Erro ao carregar o vídeo:", erro);
-        videoContainer.innerHTML = "<p>Erro ao carregar vídeo. Tente novamente mais tarde.</p>";
+    if (
+      liveData.items &&
+      liveData.items.length > 0 &&
+      liveData.items[0].id &&
+      liveData.items[0].id.videoId
+    ) {
+      const liveId = liveData.items[0].id.videoId;
+      videoContainer.innerHTML = `
+        <iframe width="100%" height="400" src="https://www.youtube.com/embed/${liveId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <p style="text-align:center; color:green; font-weight: bold;">🔴 AO VIVO</p>
+      `;
+      return; // Sai da função pois já carregou o vídeo ao vivo
     }
+
+    // 2. Se não tem vídeo ao vivo, buscar o último vídeo postado
+    const lastVideoURL = `https://www.googleapis.com/youtube/v3/search?part=snippet,id&channelId=${channelId}&maxResults=1&order=date&type=video&key=${apiKey}`;
+    const lastResponse = await fetch(lastVideoURL);
+    const lastData = await lastResponse.json();
+    console.log("Resposta último vídeo:", lastData);
+
+    if (
+      lastData.items &&
+      lastData.items.length > 0 &&
+      lastData.items[0].id &&
+      lastData.items[0].id.videoId
+    ) {
+      const videoId = lastData.items[0].id.videoId;
+      videoContainer.innerHTML = `
+        <iframe width="100%" height="400" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <p style="text-align:center; color:gray; font-weight: bold;">⚪ Última Transmissão</p>
+      `;
+    } else {
+      // Nenhum vídeo encontrado
+      videoContainer.innerHTML = "<p style='text-align:center; color:#777;'>Nenhum vídeo encontrado no canal.</p>";
+    }
+  } catch (erro) {
+    console.error("Erro ao carregar o vídeo:", erro);
+    videoContainer.innerHTML = "<p style='text-align:center; color:red;'>Erro ao carregar vídeo. Tente novamente mais tarde.</p>";
+  }
 }
 
 document.addEventListener("DOMContentLoaded", carregarVideo);
